1a54c67412f9bb73fd96aec628ad2c5148812e78
Hiago Franco <hiagofranco2604@gmail.com>
2025-06-23 19:00:05 +0100

    ex02_AlmostFinished

---
+++ ex02_SquareRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿/*2. Create a method named “SquareRoot” that receives one numeric input (double) and
+returns the mathematical square root value of the same. The number must be a
+decimal with two decimal places;*/
+
+using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, digite um numero e eu lhe darei a raiz quadrada do mesmo:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"A raiz quadrada de {number} é {SquareRoot(number)}.");
+
+        }
+        static double SquareRoot(double number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+}
