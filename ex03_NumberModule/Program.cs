88db8bc958d4c3cb5cce3c4e64b765292292edca
Hiago Franco <hiago.franco@students.assembly.pt>
2025-06-23 21:01:19 +0100

    ex03_AlmostFinished

---
+++ ex03_NumberModule/Program.cs
@@ -0,0 +1,19 @@
+﻿/*3. Create a method named “NumberModule” that receives one numeric input (double)
+and returns the module value of the same;*/
+
+using System;
+
+namespace NumberModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, tell me a number and i´ll return you if your number is even or odd:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double result = number % 2;
+            if(result == 0)
+                Console.WriteLine
+        }
+    }
+}
